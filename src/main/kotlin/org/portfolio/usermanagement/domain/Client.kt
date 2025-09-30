b421790999ceb691bf8750143cdd18233fc97b7e
Dante6327 <jinpyeong93@gmail.com>
2025-10-01 06:54:45 +0900

    엔티티 생성 중간 커밋

---
+++ src/main/kotlin/org/portfolio/usermanagement/domain/Client.kt
@@ -0,0 +1,26 @@
+package org.portfolio.usermanagement.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+// SDK를 설치하는 서비스사를 구분하는 엔티티
+// API Key로 구분
+@Entity
+@Table(name = "client")
+class Client (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column
+    val companyName: String,
+
+    @Column
+    val apiKey: String, //api key - 추후 해시 알고리즘으로 단방향 암호화 하여 DB 저장
+){
+}
---
+++ src/main/kotlin/org/portfolio/usermanagement/domain/Event.kt
@@ -0,0 +1,4 @@
+package org.portfolio.usermanagement.domain
+
+class Event {
+}
